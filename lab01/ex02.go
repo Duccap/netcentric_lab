15da78bf7907d8fc3156ba5ef3072a9473724333
Tunizrel <84724694+Tunizrel@users.noreply.github.com>
2024-10-11 03:35:52 +0700

    Update ex02.go

--- lab01/ex02.go
+++ lab01/ex02.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func ex02() {
-	fmt.Println("Hello, World!")
+func wordScore(letter string) int {
+	switch letter {
+	case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
+		return 1
+	case "D", "G":
+		return 2
+	case "B", "C", "M", "P":
+		return 3
+	case "F", "H", "V", "W", "Y":
+		return 4
+	case "K":
+		return 5
+	case "J", "X":
+		return 8
+	case "Q", "Z":
+		return 10
+	default:
+		return 0
+	}
+}
+
+
+func calculateWord(word string) int {
+	total := 0
+	for _, letter := range word {
+		total += wordScore(strings.ToUpper(string(letter)))
+	}
+	return total
+}
+
+func main() {
+	ex := "good day"
+	fmt.Println(ex)
+	fmt.Println("Total score = ", calculateWord(ex))
 }
